4ff8e328656c95939e484583d30849f118ac3e78
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2019-02-04 14:28:44 -0500

    Feat: Add parser class and stemmer class and add binary search

---
+++ src/ParserA.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+/**
+ * ISTE-612 LBE03 Text processing
+ * Prof Kang
+ */
+
+public class ParserA {
+    private String[] myDocs;
+
+    String[] stopWords = {"a","is","in","so","of", "at", "the","to","and","it","as",
+            "be","are","on","into","if","it's"};
+
+    public ParserA() {
+        Arrays.sort(stopWords);
+    }
+
+    //Binary search for a stop word
+    //Return -1 if stop word found else return +1
+    public int searchStopWord(String key) {
+        int lowerIndex = 0;
+        int higherIndex = stopWords.length-1;
+
+        while(lowerIndex < higherIndex) {
+            int mid = lowerIndex + higherIndex-lowerIndex / 2;
+            int result = key.compareTo(stopWords[mid]);
+            if(result < 0) {
+                higherIndex = mid -1;
+            }
+            else if (result > 0) {
+                lowerIndex = mid+1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    //Tokenization
+    public ArrayList<String> parse(File fileName) throws IOException {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        ParserA test = new ParserA();
+        System.out.println("Stop word: " + test.searchStopWord("are"));
+    }
+}
